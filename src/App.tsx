36df671958daabc7e09abc1a21bfbf3d64068ec6
Anton Iskryk <iskryk.anton@gmail.com>
2023-03-01 15:10:03 +0200

    add router and make paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
-function App() {
+const Layout = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Navbar />
+      <Sidebar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      }
+    ]
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
+
+function App () {
+  return (
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+// import { AuthContext } from '../context/authContext';
+// import { login } from '../../../api/controllers/auth.js';
+
+const Login = () => {
+  // const [inputs, setInputs] = useState({
+  //   username: '',
+  //   password: '',
+  // });
+
+  // const [error, setError] = useState(null);
+
+  // const navigate = useNavigate();
+
+  // const { currentUser } = useContext(AuthContext);
+
+  // const handleChange = (e) => {
+  //   setInputs(prev => ({
+  //     ...prev,
+  //     [e.target.name]: e.target.value,
+  //   }));
+  // };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   try {
+  //     await login(inputs);
+  //     navigate('/');
+  //   } catch (err) {
+  //     console.log(err);
+  //     setError(err.response.data);
+  //   }
+  // };
+
+  return (
+    <div>Login</div>
+    // <div className='auth'>
+    //   <h1>Login</h1>
+    //   <form>
+    //     <input
+    //       type="text"
+    //       placeholder='username'
+    //       name='username'
+    //       onChange={handleChange}
+    //     />
+    //     <input
+    //       type="password"
+    //       placeholder='password'
+    //       name='password'
+    //       onChange={handleChange}
+    //     />
+    //     <button onClick={handleSubmit}>
+    //       Login
+    //     </button>
+
+    //     {error && <p>{error}</p>}
+
+    //     <span>
+    //       Don't you have an account? <Link to='/register'>Register</Link>
+    //     </span>
+    //   </form>
+    // </div>
+  );
+};
+
+export default Login;
