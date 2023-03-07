a29e806a6e568582c0f5fcf55c511070df43f783
Anton Iskryk <iskryk.anton@gmail.com>
2023-03-07 22:30:04 +0200

    align loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,17 +1,27 @@
 import { ThreeDots } from  'react-loader-spinner'
+import styled from 'styled-components';
+
+const LoaderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Loader = () => {
   return (
-    <ThreeDots 
-      height="80" 
-      width="80" 
-      radius="9"
-      color="#593c87" 
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{}}
-      wrapperClass=''
-      visible={true}
-    />
+    <LoaderContainer>
+      <ThreeDots 
+        height="80"
+        width="80"
+        radius="9"
+        color="#593c87"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=''
+        visible={true}
+      />
+    </LoaderContainer>
   );
 };
 
