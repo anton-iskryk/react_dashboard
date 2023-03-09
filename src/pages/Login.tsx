04e56abbf9300311225b71fbc8051c7ec0f3044a
Anton Iskryk <iskryk.anton@gmail.com>
2023-03-09 23:24:06 +0200

    create UI and router for register and login pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,68 +1,159 @@
-import React from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-// import { AuthContext } from '../context/authContext';
-// import { login } from '../../../api/controllers/auth.js';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #242731;
+`;
+
+const LoginTitle = styled.h1`
+  margin-bottom: 8px;
+
+  font-weight: 500;
+  font-size: 48px;
+  line-height: 56px;
+
+  color: #FFFFFF;
+`;
+
+const LoginDescription = styled.p`
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 24px;
+
+  color: #9496A6;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+`;
+
+const InputsContainer = styled.div`
+  margin: 48px 0;
+`;
+
+const LoginInput = styled.input`
+  width: 100%;
+  padding: 20px;
+
+  border: none;
+  border: 1px solid #3D4250;
+  border-radius: 8px;
+
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 24px;
+
+  color: #7D7F8E;
+  background-color: transparent;
+  outline: none;
+
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+
+const LoginButton = styled.button`
+  padding: 20px;
+  margin-bottom: 48px;
+
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 24px;
+
+  background-color: #8941FF;
+  color: #fff;
+  border: 1px solid #8941FF;
+  border-radius: 8px;
+
+  cursor: pointer;
+  transition: background-color .3s;
+
+  &:hover {
+    background-color: transparent;
+  }
+`;
+
+const Span = styled.span`
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 24px;
+
+  color: #fff;
+  text-align: center;
+`;
+
+const RegisterLink = {
+  fontWeight: '500',
+  fontSize: '15px',
+  lineHeight: '24px',
+  color: '#78B4ED',
+  textDecoration: 'none',
+};
 
 const Login = () => {
-  // const [inputs, setInputs] = useState({
-  //   username: '',
-  //   password: '',
-  // });
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
 
   // const [error, setError] = useState(null);
 
   // const navigate = useNavigate();
 
-  // const { currentUser } = useContext(AuthContext);
 
-  // const handleChange = (e) => {
-  //   setInputs(prev => ({
-  //     ...prev,
-  //     [e.target.name]: e.target.value,
-  //   }));
-  // };
+  const handleChange = (e: { target: { name: string; value: string; }; }) => {
+    setInputs(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-
-  //   try {
-  //     await login(inputs);
-  //     navigate('/');
-  //   } catch (err) {
-  //     console.log(err);
-  //     setError(err.response.data);
-  //   }
-  // };
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+  };
 
   return (
-    <div>Login</div>
-    // <div className='auth'>
-    //   <h1>Login</h1>
-    //   <form>
-    //     <input
-    //       type="text"
-    //       placeholder='username'
-    //       name='username'
-    //       onChange={handleChange}
-    //     />
-    //     <input
-    //       type="password"
-    //       placeholder='password'
-    //       name='password'
-    //       onChange={handleChange}
-    //     />
-    //     <button onClick={handleSubmit}>
-    //       Login
-    //     </button>
+    <LoginContainer>
+      <LoginTitle>Welcome back</LoginTitle>
+      <LoginDescription>Welcome back! Please enter your details</LoginDescription>
+      <LoginForm>
+        <InputsContainer>
+          <LoginInput
+            required
+            type="email"
+            placeholder='Email'
+            name='email'
+            onChange={handleChange}
+          />
 
-    //     {error && <p>{error}</p>}
+          <LoginInput
+            required
+            type="password"
+            placeholder='Password'
+            name='password'
+            onChange={handleChange}
+          />
+        </InputsContainer>
 
-    //     <span>
-    //       Don't you have an account? <Link to='/register'>Register</Link>
-    //     </span>
-    //   </form>
-    // </div>
+        <LoginButton
+          onClick={handleSubmit}
+        >
+          Log in
+        </LoginButton>
+
+        {/* {error && <p>{error}</p>} */}
+
+        <Span>Or <Link style={RegisterLink} to='/react_dashboard/register/'>Sign up</Link></Span>
+      </LoginForm>
+    </LoginContainer>
   );
 };
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,72 +1,173 @@
-import React from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import axios from 'axios';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const RegisterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #242731;
+`;
+
+const RegisterTitle = styled.h1`
+  margin-bottom: 8px;
+
+  font-weight: 500;
+  font-size: 48px;
+  line-height: 56px;
+
+  color: #FFFFFF;
+`;
+
+const RegisterDescription = styled.p`
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 24px;
+
+  color: #9496A6;
+`;
+
+const RegisterForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+`;
+
+const InputsContainer = styled.div`
+  margin: 48px 0;
+`;
+
+const RegisterInput = styled.input`
+  width: 100%;
+  padding: 20px;
+
+  border: none;
+  border: 1px solid #3D4250;
+  border-radius: 8px;
+
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 24px;
+
+  color: #7D7F8E;
+  background-color: transparent;
+  outline: none;
+
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+
+const RegisterButton = styled.button`
+  padding: 20px;
+  margin-bottom: 48px;
+
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 24px;
+
+  background-color: #8941FF;
+  color: #FFFFFF;
+  border: 1px solid #8941FF;
+  border-radius: 8px;
+
+  cursor: pointer;
+  transition: background-color .3s;
+
+  &:hover {
+    background-color: transparent;
+  }
+`;
+
+const Span = styled.span`
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 24px;
+
+  color: #fff;
+  text-align: center;
+`;
+
+const LoginLink = {
+  fontWeight: '500',
+  fontSize: '15px',
+  lineHeight: '24px',
+  color: '#78B4ED',
+  textDecoration: 'none',
+};
 
 const Register = () => {
-  // const [inputs, setInputs] = useState({
-  //   username: '',
-  //   email: '',
-  //   password: '',
-  // });
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
-  // const [error, setError] = useState(null);
+  const [error, setError] = useState(null);
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const handleChange = (e: { target: { name: string; value: string; }; }) => {
-  //   setInputs(prev => ({
-  //     ...prev,
-  //     [e.target.name]: e.target.value,
-  //   }));
-  // };
+  const handleChange = (e: { target: { name: string; value: string; }; }) => {
+    setInputs(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-  // const handleSubmit = async (e: { preventDefault: () => void; }) => {
-  //   e.preventDefault();
-
-  //   try {
-  //     await axios.post('/auth/register', inputs);
-  //     navigate('/login');
-  //   } catch (err: any) {
-  //     console.log(err);
-  //     setError(err.response.data);
-  //   }
-  // };
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+  };
 
   return (
-    <div>Register</div>
-    // <div className='auth'>
-    //   <h1>Register</h1>
-    //   <form>
-    //     <input
-    //       required
-    //       type="text"
-    //       placeholder='username'
-    //       name='username'
-    //       onChange={handleChange}
-    //     />
-    //     <input
-    //       required
-    //       type="email"
-    //       placeholder='email'
-    //       name='email'
-    //       onChange={handleChange}
-    //     />
-    //     <input
-    //       required
-    //       type="password"
-    //       placeholder='password'
-    //       name='password'
-    //       onChange={handleChange}
-    //     />
-    //     <button
-    //       onClick={handleSubmit}
-    //     >
-    //       Register
-    //     </button>
-    //     {error && <p>{error}</p>}
-    //     <span>Do you have an account? <Link to='/login'>Login</Link></span>
-    //   </form>
-    // </div>
+    <RegisterContainer>
+      <RegisterTitle>Welcome</RegisterTitle>
+      <RegisterDescription>Welcome! Please enter your details</RegisterDescription>
+      <RegisterForm>
+        <InputsContainer>
+          <RegisterInput
+            required
+            type="text"
+            placeholder='Name'
+            name='name'
+            onChange={handleChange}
+          />
+
+          <RegisterInput
+            required
+            type="email"
+            placeholder='Email'
+            name='email'
+            onChange={handleChange}
+          />
+
+          <RegisterInput
+            required
+            type="password"
+            placeholder='Password'
+            name='password'
+            onChange={handleChange}
+          />
+        </InputsContainer>
+
+        <RegisterButton
+          onClick={handleSubmit}
+        >
+          Create account
+        </RegisterButton>
+
+        {error && <p>{error}</p>}
+
+        <Span>Or <Link
+            style={LoginLink}
+            to='/react_dashboard/login/'
+          >
+            Log in
+          </Link>
+        </Span>
+      </RegisterForm>
+    </RegisterContainer>
   );
 };
 
